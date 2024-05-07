ff720be61a0c781efc928eeb4bcdcf680c181a8d
sweti-singh <sweti.singh1227@gmail.com>
2024-05-07 23:45:53 +0530

    seccond commit

--- app/index.js
+++ app/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const dotenv = require('dotenv')
-const connection= require('./url/dbconnect.js')
+const connection= require('../url/dbconnect.js')
 
 //dotenv config
 dotenv.config()
@@ -17,7 +17,7 @@ app.use(cors({
     origin: '*',  
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' 
   }))
-app.use('/api/v1/portfolio',require('./routes/portfolioRoutes'))
+app.use('/api/v1/portfolio',require('../routes/portfolioRoutes.js'))
 
 // //route
 // app.get('/',(req,res)=>{
